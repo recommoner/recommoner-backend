4e2140fd16c6749d6619435b59d255d43860528e
Suprita Shankar <suprita.shankar@gmail.com>
2017-05-11 14:39:53 -0700

    add the recom models

--- recommonerserver/recommoner/models.py
+++ recommonerserver/recommoner/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    RECOM_ROLES = (
+        ('SC', 'Stewarding Commoner'),
+        ('EC', 'Exploiting Commoner')
+    )
+    """
+        Django user model - first_name, last_name, email, password,
+        groups, user_permissions
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=300)
+    organization = models.CharField(max_length=400)
+    recom_roles = models.CharField(
+        max_length=10,
+        choices=RECOM_ROLES,
+        default='SC'
+    )
