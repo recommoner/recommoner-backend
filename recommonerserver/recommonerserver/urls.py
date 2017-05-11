bd5696e8501cabf3ee8cfabbcccb52a79be6de9e
Suprita Shankar <suprita.shankar@gmail.com>
2017-05-11 15:54:57 -0700

    add rest serialization to view data as json

--- recommonerserver/recommonerserver/urls.py
+++ recommonerserver/recommonerserver/urls.py
@@ -1,22 +1,27 @@
-"""recommonerserver URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+"""recommonerserver URL Configuration"""
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+from recommoner.models import UserProfile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'bio', 'organization')
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserProfileViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^recom/', include('recommoner.urls'))
+    url(r'^recom/', include('recommoner.urls')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
